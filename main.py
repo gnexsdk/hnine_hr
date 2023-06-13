f6e997f48a993763cc342b1a130b64f80b3330d0
gnexsdk <gnexsdk@gmail.com>
2023-06-13 21:08:24 +0900

    app 호스팅 주소 주석 실수 수정

--- main.py
+++ main.py
@@ -184,5 +184,5 @@ def process_xlsx(df):
 
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port=5001)
-    app.run(port=5001)
+    app.run(host="0.0.0.0", port=5001)
+    #app.run(port=5001)
